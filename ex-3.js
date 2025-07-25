f86fa1d37ee286198843ce0055aeb8f68ac9a7af
plawan1995 <jnyjame12@gmail.com>
2025-07-25 12:44:58 +0700

    feat: implement functions to process word lengths, filter 5-char words, and manage todo items

--- ex-3.js
+++ ex-3.js
@@ -1,24 +1,27 @@
 const todos = [
-  { topic: "Doing pre-work", completed: true },
-  { topic: "Workout", completed: false },
-  { topic: "Playing computer games", completed: true },
-  { topic: "Relax", completed: false },
-  { topic: "Clean the room", completed: true },
-];
-
-function getTodoTopics(todos) {
-  // Start coding here
-}
-
-getTodoTopics(todos);
-
-/* 
-	Output:
-	[
-		'Doing pre-work',
-		'Workout',
-		'Playing computer games',
-		'Relax',
-		'Clean the room'
-	]
-*/
+	{ topic: "Doing pre-work", completed: true },
+	{ topic: "Workout", completed: false },
+	{ topic: "Playing computer games", completed: true },
+	{ topic: "Relax", completed: false },
+	{ topic: "Clean the room", completed: true },
+  ];
+  
+  function getTodoTopics(todos) {
+	// Start coding here
+  return todos.map(todo => todo.topic);
+  }
+  
+  getTodoTopics(todos);
+  console.log(getTodoTopics(todos))
+  
+  /* 
+	  Output:
+	  [
+		  'Doing pre-work',
+		  'Workout',
+		  'Playing computer games',
+		  'Relax',
+		  'Clean the room'
+	  ]
+  */
+  
